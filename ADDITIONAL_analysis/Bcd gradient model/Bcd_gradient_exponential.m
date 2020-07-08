5ea2022f0a5381988a1dfaf2fdb4265ff06b631f
Huy Tran <huytran.hn@gmail.com>
2020-07-08 12:15:32 +0200

    good w exponential, adding stuffs to explain power function

--- ADDITIONAL_analysis/Bcd gradient model/Bcd_gradient_exponential.m
+++ ADDITIONAL_analysis/Bcd gradient model/Bcd_gradient_exponential.m
@@ -1,4 +1,4 @@
-function [c0_grid] = Bcd_gradient(xax,gridt,D,L)
+function [c0_grid] = Bcd_exponential_gradient(xax,gridt,D,L)
 % xax: position axes (in %EL)
 % tax: time axes (in s)
 % D: diffusion coefficient (unit in micron^2/s)
@@ -14,25 +14,26 @@ function [c0_grid] = Bcd_gradient(xax,gridt,D,L)
     dt =0.001;
     gridx = 0:dx:500;
     gridt = 0:dt:1000;
+    % Predicted lambda:
+    lambda = sqrt(D*L);
     % set initial condition:
-    cax = gridx*0; cax(1) = 1;
+    cax = exp(-gridx/lambda);
     %cax_rec = zeros(numel(gridt),numel(gridx));
     dcax = gridx*0;
     d2cax = gridx*0;
-    % Predicted lambda:
-    lambda = sqrt(D*L);
+    
     semilogy(xax,exp(-xax/lambda),'--');
     hold on;
     
     for tidx = 2:numel(gridt)
         % Calculate decay:
-        for xidx = 1:numel(xax)
+        for xidx = 1:numel(gridx)
             dcax(xidx) = -cax(xidx)/L;
         end
         % Birth only at anterior pole
-        dcax(1) = dcax(1)+0.1/dt;
+        dcax(1) = dcax(1)+1;
         % Calculate 2nd derivatives over x:
-        for xidx = 2:numel(xax)-1
+        for xidx = 2:numel(gridx)-1
             d2cax(xidx) = D*(cax(xidx-1)+cax(xidx+1)-2*cax(xidx))/2/dx.^2;
         end
         d2cax(1) = D*(+cax(2)-cax(1))/dx.^2;
@@ -41,8 +42,8 @@ function [c0_grid] = Bcd_gradient(xax,gridt,D,L)
         cax = cax + dt*(dcax+d2cax);
         % Record:
         %cax_rec = [cax_rec;cax];
-        if mod(tidx,1000)==2
-            semilogy(gridx,cax+1e-5);
+        if mod(tidx,10000)==2
+            semilogy(gridx,cax+1e-10);
             hold on;
         end
     end
