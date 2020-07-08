5f48fe7868cbf408daa2cbc218a2b5493540dbf5
Huy Tran <huytran.hn@gmail.com>
2020-07-08 12:20:14 +0200

    Update Bcd_gradient_multistep_decay.m

--- ADDITIONAL_analysis/Bcd gradient model/Bcd_gradient_multistep_decay.m
+++ ADDITIONAL_analysis/Bcd gradient model/Bcd_gradient_multistep_decay.m
@@ -1,4 +1,4 @@
-function [c0_grid] = Bcd_exponential_gradient(xax,gridt,D,L)
+function [c0_grid] = Bcd_gradient_multistep_decay(xax,gridt,D,L)
 % xax: position axes (in %EL)
 % tax: time axes (in s)
 % D: diffusion coefficient (unit in micron^2/s)
@@ -17,10 +17,14 @@ function [c0_grid] = Bcd_exponential_gradient(xax,gridt,D,L)
     % Predicted lambda:
     lambda = sqrt(D*L);
     % set initial condition:
-    cax = exp(-gridx/lambda);
-    %cax_rec = zeros(numel(gridt),numel(gridx));
-    dcax = gridx*0;
-    d2cax = gridx*0;
+    cax_1 = exp(-gridx/lambda);
+    dcax_1 = gridx*0;
+    d2cax_1 = gridx*0;
+    
+    cax_2 = exp(-gridx/lambda);
+    dcax_2 = gridx*0;
+    d2cax_2 = gridx*0;
+    
     
     semilogy(xax,exp(-xax/lambda),'--');
     hold on;
@@ -28,25 +32,30 @@ function [c0_grid] = Bcd_exponential_gradient(xax,gridt,D,L)
     for tidx = 2:numel(gridt)
         % Calculate decay:
         for xidx = 1:numel(gridx)
-            dcax(xidx) = -cax(xidx)/L;
+            dcax_1(xidx) = -cax_1(xidx)/L;
+            dcax_2(xidx) = +cax_1(xidx)/L;
         end
         % Birth only at anterior pole
-        dcax(1) = dcax(1)+1;
+        dcax_1(1) = dcax_1(1)+1;
         % Calculate 2nd derivatives over x:
         for xidx = 2:numel(gridx)-1
-            d2cax(xidx) = D*(cax(xidx-1)+cax(xidx+1)-2*cax(xidx))/2/dx.^2;
+            d2cax_1(xidx) = D*(cax_1(xidx-1)+cax_1(xidx+1)-2*cax_1(xidx))/2/dx.^2;
+            d2cax_2(xidx) = D*(cax_2(xidx-1)+cax_2(xidx+1)-2*cax_2(xidx))/2/dx.^2;
         end
-        d2cax(1) = D*(+cax(2)-cax(1))/dx.^2;
-        d2cax(end) = D*(-cax(end)+cax(end-1))/dx.^2;
+        d2cax_1(1) = D*(+cax_1(2)-cax_1(1))/dx.^2;
+        d2cax_1(end) = D*(-cax_1(end)+cax_1(end-1))/dx.^2;
+        d2cax_2(1) = D*(+cax_2(2)-cax_2(1))/dx.^2;
+        d2cax_2(end) = D*(-cax_2(end)+cax_2(end-1))/dx.^2;
         % Final: 
-        cax = cax + dt*(dcax+d2cax);
+        cax_1 = cax_1 + dt*(dcax_1+d2cax_1);
+        cax_2 = cax_2 + dt*(dcax_2+d2cax_2);
         % Record:
         %cax_rec = [cax_rec;cax];
         if mod(tidx,10000)==2
-            semilogy(gridx,cax+1e-10);
+            semilogy(gridx,cax_2+1e-10);
             hold on;
         end
     end
     
     % Final: 
-    c0_grid = cax;
+    c0_grid = cax_1;
