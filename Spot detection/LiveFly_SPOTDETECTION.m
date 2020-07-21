4da5375e9a9d0474947d2587fcab27c2c8c93dda
Huy Tran <huytran.hn@gmail.com>
2020-07-21 11:22:19 +0200

    Revert "changes for nothing"
    This reverts commit 0d7b0d64cdf43b0e6e26bef892f8f9bc7e5bd091.

--- Spot detection/LiveFly_SPOTDETECTION.m
+++ Spot detection/LiveFly_SPOTDETECTION.m
@@ -12,12 +12,11 @@ nuclei_mov='RED';                                   %title of segmented nuclei m
 seg_init = 1;
 seg_end  = 380;
 %select the frame range you want to analyze
-it_start=[1];
-it_end=[380];
+it_start=[250];
+it_end=[260];
 %% Parameters:
 ConfigName=0;
-inptmp = questdlg('Choose a configuration file?');
-if strcmp(inptmp,'Yes')
+if strcmp(questdlg('Choose a configuration file?'),'Yes')
     [ConfigName,PathName,FilterIndex] = uigetfile(fullfile(mov_folder,'*.m'),'Select the configuration file');
     run(fullfile(PathName,ConfigName));
 else
@@ -43,15 +42,11 @@ else
     fact_r=1.2;             % tolerance radius (if fact_r=1) the distance is equal to the radius. fact_r for histone is generally bigger than 1.
 end
 %% Ask some question about debug mode
-inptmp = questdlg('Enter debug mode to find th1, th2?');
-switch inptmp
-    case 'Yes'
-        th=[0 0];
-        answer=inputdlg('Enter a frame number with spots');
-        it_start=str2double(answer);
-        it_end=it_start;
-    case 'Cancel'
-        return;
+if strcmp(questdlg('Enter debug mode to find th1, th2?'),'Yes')
+    th=[0 0];
+    answer=inputdlg('Enter a frame number with spots');
+    it_start=str2double(answer);
+    it_end=it_start;
 end
 %% Brightness adjustment after spot detection
 brightness_ratio=[1.2 1.2]; % Amplify the (1) green and (2) red intensity for better visualization
